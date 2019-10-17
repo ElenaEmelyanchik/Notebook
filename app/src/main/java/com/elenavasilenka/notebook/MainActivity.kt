c562ad01d7d0cc10bc6f35c09cde4186ba6617f5
Vasilenka Lena <emelyanchik.elena@gmail.com>
2019-10-17 07:19:31 +0200

    add textview

--- app/src/main/java/com/elenavasilenka/notebook/MainActivity.kt
+++ app/src/main/java/com/elenavasilenka/notebook/MainActivity.kt
@@ -1,22 +1,56 @@
 package com.elenavasilenka.notebook
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
 
+    lateinit var textView: TextView
+    var text: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        text = savedInstanceState?.getString(TEXT)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.text)
         Log.d(TAG, "onCreate")
     }
 
     override fun onStart(){
         super.onStart()
         Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    companion object{
+        private const val TEXT = "text"
     }
 }
